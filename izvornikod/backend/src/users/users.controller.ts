103f920f04048335eead8f3b14e6fef414fe0f58
karlokus <karlo.kus@gmail.com>
2026-04-06 15:59:48 +0200

    add changes to dokumentacija and add start of code

---
+++ izvornikod/backend/src/users/users.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from "@nestjs/common";
+import { UsersService } from "./providers/users.service";
+
+@Controller('users')
+export class UsersController {
+
+    constructor(usersService: UsersService) {}
+}
---
+++ izvornikod/backend/src/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./providers/users.service";
+
+
+@Module({
+    controllers: [UsersController],
+    providers: [UsersService],
+    imports: [],
+    exports: [],
+})
+export class UsersModule {}
